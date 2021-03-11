cdafe9719afeec15b9f745b16f35e12481831533
ProjectPB <73194192+ProjectPB@users.noreply.github.com>
2021-03-11 21:51:02 +0100

    Card improvements

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,26 @@
+import { FavoriteOutlined } from "@material-ui/icons";
 import React from "react";
 import "./Card.css";
 
 function Card({ user, image, title, UserImage }) {
-  return (
-    <div className="card">
-      <img src={image} alt="" />
-      <div className="card__info">
-        <h1>{title}</h1>
-      <div className="card__user">
-        <UserImage />
-        <p>{user}</p>
-      </div>
-      </div>
-    </div>
-  );
+    return (
+        <div className="card">
+            <img src={image} alt="" />
+            <div className="card__info">
+                <h1>{title}</h1>
+                <div className="card__right">
+                    <div className="card__favorite">
+                        <FavoriteOutlined />
+                        <p>345</p>
+                    </div>
+                    <div className="card__user">
+                        <UserImage />
+                        <p>{user}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Card;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,45 +1,66 @@
-import React, { useState } from "react";
+import {
+    Favorite,
+    Home,
+    LibraryAdd,
+    MenuBook,
+    Whatshot,
+} from "@material-ui/icons";
+import React from "react";
 import "./Sidebar.css";
-import HomeIcon from '@material-ui/icons/Home';
-import WhatshotIcon from '@material-ui/icons/Whatshot';
-import MenuBookIcon from '@material-ui/icons/MenuBook';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import LibraryAddIcon from '@material-ui/icons/LibraryAdd';
-
 
 function Sidebar({ hidden }) {
-  return (
-    <div className={hidden ? "sidebar--hidden" : "sidebar"}>
-      <div className={hidden ? "sidebar__option--hidden" : "sidebar__option"}>
-        <HomeIcon fontSize="large" />
-        <h1>MAIN</h1>
-      </div>
+    return (
+        <div className={hidden ? "sidebar--hidden" : "sidebar"}>
+            <div
+                className={
+                    hidden ? "sidebar__option--hidden" : "sidebar__option"
+                }
+            >
+                <Home fontSize="large" />
+                <h1>MAIN</h1>
+            </div>
 
-      <div className={hidden ? "sidebar__option--hidden" : "sidebar__option"}>
-        <WhatshotIcon fontSize="large" />
-        <h1>POPULAR</h1>
-      </div>
+            <div
+                className={
+                    hidden ? "sidebar__option--hidden" : "sidebar__option"
+                }
+            >
+                <Whatshot fontSize="large" />
+                <h1>POPULAR</h1>
+            </div>
 
-      <hr />
+            <hr />
 
-      <div className={hidden ? "sidebar__option--hidden" : "sidebar__option"}>
-        <MenuBookIcon fontSize="large" />
-        <h1>CREATED BY ME</h1>
-      </div>
+            <div
+                className={
+                    hidden ? "sidebar__option--hidden" : "sidebar__option"
+                }
+            >
+                <MenuBook fontSize="large" />
+                <h1>CREATED BY ME</h1>
+            </div>
 
-      <div className={hidden ? "sidebar__option--hidden" : "sidebar__option"}>
-        <FavoriteIcon fontSize="large" />
-        <h1>FAVORITE</h1>
-      </div>
+            <div
+                className={
+                    hidden ? "sidebar__option--hidden" : "sidebar__option"
+                }
+            >
+                <Favorite fontSize="large" />
+                <h1>FAVORITE</h1>
+            </div>
 
-      <hr />
+            <hr />
 
-      <div className={hidden ? "sidebar__option--hidden" : "sidebar__option"}>
-        <LibraryAddIcon fontSize="large"/>
-        <h1>COLLECTIONS</h1>
-      </div>
-    </div>
-  );
+            <div
+                className={
+                    hidden ? "sidebar__option--hidden" : "sidebar__option"
+                }
+            >
+                <LibraryAdd fontSize="large" />
+                <h1>COLLECTIONS</h1>
+            </div>
+        </div>
+    );
 }
 
 export default Sidebar;
