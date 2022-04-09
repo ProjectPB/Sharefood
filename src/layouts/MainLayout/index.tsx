07690bb495dff83d4ed407bb2245752e8d6b7acd
ProjectPB <73194192+ProjectPB@users.noreply.github.com>
2022-04-09 22:50:21 +0200

    Implement simple pull-to-refresh functionality in main layout

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import PullToRefresh from "react-simple-pull-to-refresh";
 import { Outlet } from "react-router";
 import { useWidth } from "../../hooks";
+import { RefreshOutlined } from "@material-ui/icons";
 
 import Header from "../../components/Header";
 import SearchBar from "../../components/SearchBar";
@@ -11,16 +13,39 @@ import "./styles.css";
 const MainLayout: React.FC = () => {
   const width = useWidth();
 
+  const handleRefresh = () => {
+    return new Promise((resolve, reject) => {
+      try {
+        resolve(
+          window.location.reload()
+        )
+      } catch (err) {
+        reject(err.message)
+      }
+
+    })
+  }
+
   return (
     <div className="mainLayout">
       <Header />
       {width <= 600 && <SearchBar />}
       <div className="main">
         <Sidebar />
-        <Outlet />
-      </div>
+        <div className="main__container">
+          <PullToRefresh
+            onRefresh={handleRefresh}
+            pullingContent=""
+            refreshingContent={
+              <div className="main__refresh">
+                <RefreshOutlined fontSize="large" />
+              </div>}>
+            <Outlet />
+          </PullToRefresh>
+        </div>
+      </div >
 
-    </div>
+    </div >
   );
 };
 
