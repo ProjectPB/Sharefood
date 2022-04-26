3b8ad4e49ef057d2e57c2b5fe44580942779b94f
ProjectPB <73194192+ProjectPB@users.noreply.github.com>
2022-04-26 18:41:12 +0200

    Fix styling in new recipe form and recipe filters

--- src/components/RecipesRenderer/index.tsx
+++ src/components/RecipesRenderer/index.tsx
@@ -110,40 +110,42 @@ const RecipesRenderer: React.FC<Props> = ({ filters, typesAvailable, changeType,
     >
       <div ref={topRef} />
 
-      <div className="recipesRenderer__filtersContainer">
-        {statsAvailable &&
-          <div className="recipesRenderer__filters">
-            <p>Sort by:</p>
-            <div className="recipesRenderer__buttons">
-              {statsFilters.map((({ value, name }, id) => (
-                <button
-                  key={id}
-                  className={filters.statsFilter === value ? "active" : undefined}
-                  onClick={() => changeStats(value)}
-                >
-                  {name}
-                </button>
-              )))}
+      {(statsAvailable || typesAvailable) &&
+        <div className="recipesRenderer__filtersContainer">
+          {statsAvailable &&
+            <div className="recipesRenderer__filters">
+              <p>Sort by:</p>
+              <div className="recipesRenderer__buttons">
+                {statsFilters.map((({ value, name }, id) => (
+                  <button
+                    key={id}
+                    className={filters.statsFilter === value ? "active" : undefined}
+                    onClick={() => changeStats(value)}
+                  >
+                    {name}
+                  </button>
+                )))}
+              </div>
             </div>
-          </div>
-        }
-        {typesAvailable &&
-          <div className="recipesRenderer__filters">
-            <p>Type:</p>
-            <div className="recipesRenderer__buttons">
-              {typeFilters.map((({ value, name }, id) => (
-                <button
-                  key={id}
-                  className={filters.typeFilter === value ? "active" : undefined}
-                  onClick={() => changeType(value)}
-                >
-                  {name}
-                </button>
-              )))}
+          }
+          {typesAvailable &&
+            <div className="recipesRenderer__filters">
+              <p>Type:</p>
+              <div className="recipesRenderer__buttons">
+                {typeFilters.map((({ value, name }, id) => (
+                  <button
+                    key={id}
+                    className={filters.typeFilter === value ? "active" : undefined}
+                    onClick={() => changeType(value)}
+                  >
+                    {name}
+                  </button>
+                )))}
+              </div>
             </div>
-          </div>
-        }
-      </div>
+          }
+        </div>
+      }
 
       {!loaded &&
         <div className="recipesRenderer__loading">
