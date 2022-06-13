84818f16a39ef2827732696ebb424e52d8cb3d4b
ProjectPB <73194192+ProjectPB@users.noreply.github.com>
2022-06-13 22:14:45 +0200

    --amend

--- src/components/SearchBar/SearchHits/Hit/index.tsx
+++ src/components/SearchBar/SearchHits/Hit/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Avatar } from '@material-ui/core';
 
 import './styles.scss';
@@ -13,13 +13,15 @@ interface Props {
 }
 
 const Hit: React.FC<Props> = (({ title, type, image, avatarSrc, hideResults, hitType }) => {
+  const [imgLoaded, setImgLoaded] = useState(false);
+
   return (
     <div className="searchHit" onClick={hideResults}>
       <div className="searchHit__data">
         <h2 className="searchHit__title">{title}</h2>
         {hitType === 'recipe' && <h3 className="searchHit__type">{type}</h3>}
       </div>
-      {hitType === 'recipe' && <img alt={title} src={image} className="searchHit__recipeImg" />}
+      {hitType === 'recipe' && <img alt={title} src={image} className={`${imgLoaded ? 'searchHit__recipeImg' : "searchHit__recipeImg--hidden"}`} onLoad={() => setImgLoaded(true)} />}
       {hitType === 'user' && <Avatar src={avatarSrc} alt={title} className="searchHit__profileImg" />}
     </div>
   )
