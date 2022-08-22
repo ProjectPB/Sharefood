6068e83f5338a923002915a1f2c0f26ddd9dae63
ProjectPB <73194192+ProjectPB@users.noreply.github.com>
2022-08-22 21:24:37 +0200

    Redux code update; use configureStore instead of createStore

--- src/redux/createStore.tsx
+++ src/redux/createStore.tsx
@@ -1,20 +1,18 @@
-import { createStore, applyMiddleware } from "redux";
 import createSagaMiddle from "redux-saga";
 import thunk from "redux-thunk";
 import { persistStore } from 'redux-persist';
-import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
+import { configureStore } from "@reduxjs/toolkit";
 
 import rootReducer from "./rootReducer";
 import rootSaga from "./rootSaga";
 
 const sagaMiddleware = createSagaMiddle();
 
-export const middlewares = [sagaMiddleware, thunk];
-
-export const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(...middlewares))
-);
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: [sagaMiddleware, thunk],
+  devTools: process.env.NODE_ENV !== 'production',
+})
 
 sagaMiddleware.run(rootSaga);
 
