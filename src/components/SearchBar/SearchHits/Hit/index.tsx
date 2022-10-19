253dc5946d9e57afd79cef69717b67f5af2041e9
ProjectPB <73194192+ProjectPB@users.noreply.github.com>
2022-10-19 21:14:50 +0200

    Recipe type translation in search results

--- src/components/SearchBar/SearchHits/Hit/index.tsx
+++ src/components/SearchBar/SearchHits/Hit/index.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import { Avatar } from '@material-ui/core';
+import { State } from '../../../../shared/types';
+import { translateType } from '../../../../shared/functions';
 
 import './styles.scss';
 
@@ -12,14 +15,19 @@ interface Props {
   hitType: string;
 }
 
+const mapState = ({ ui }: State) => ({
+  language: ui.language,
+});
+
 const Hit: React.FC<Props> = (({ title, type, image, avatarSrc, hideResults, hitType }) => {
+  const { language } = useSelector(mapState);
   const [imgLoaded, setImgLoaded] = useState(false);
 
   return (
     <div className="searchHit" onClick={hideResults}>
       <div className="searchHit__data">
         <h2 className="searchHit__title">{title}</h2>
-        {hitType === 'recipe' && <h3 className="searchHit__type">{type}</h3>}
+        {hitType === 'recipe' && <h3 className="searchHit__type">{translateType(type, language)}</h3>}
       </div>
       {hitType === 'recipe' && <img alt={title} src={image} className={`${imgLoaded ? 'searchHit__recipeImg' : "searchHit__recipeImg--hidden"}`} onLoad={() => setImgLoaded(true)} />}
       {hitType === 'user' && <Avatar src={avatarSrc} className="searchHit__profileImg" />}
